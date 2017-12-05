2ec3169a578ba9fec94a734f0a7ba91f973e205c
Richard Shi <kmshi_array@yahoo.com>
2017-12-05 23:52:06 +0800

    use async/await to rewrite

--- server/models/event.ts
+++ server/models/event.ts
@@ -12,97 +12,66 @@ export = function(Event:any) {
 	    return next(err);
 	  };
 
-	 //  app.models.Customer.findById(accessToken.userId).then((caller:any) => {
-	 //  	if (!caller || caller.roleName != ROLES.DTR){
-	 //  		var err:any = new Error('The role is wrong');
-		//     err['statusCode'] = 400;
-		//     err['code'] = 'ROLE_NAME_WRONG';
-		//     throw err;
-	 //  	}
-	 //  	ctx.args.data.dtrSenderId = caller.id;		
-		// ctx.args.data.dtrSenderName = caller.fullName;
-	 //  	return caller.company();
-	 //  }).then((company:any) => {
-	 //  	if (company){		
-		// 	ctx.args.data.companyId = company.id;
-		// 	ctx.args.data.companyName = company.name;
-		// }
-		// return app.models.Machine.upsertWithWhere(
-		// 	{dtrSenderId:ctx.args.data.dtrSenderId,MachineNumber:ctx.args.data.MachineNumber},
-		// 	ctx.args.data);
-	 //  }).then((machine:any) => {
-	 //  	return app.models.Batch.upsertWithWhere(
-		// 	{dtrSenderId:ctx.args.data.dtrSenderId,MachineNumber:ctx.args.data.MachineNumber,BatchName:ctx.args.data.BatchName},
-		// 	ctx.args.data,
-		// 	next);
-	 //  }).catch(next);
+	  var func:Function = async ()=>{
+		  try {
+			  //use 'include' filter here, so the relation function company() can work
+			  var caller: any = await app.models.Customer.findById(accessToken.userId, { include: 'company' });
+			  if (!caller || caller.roleName != ROLES.DTR) {
+				  var err: any = new Error('The role is wrong');
+				  err['statusCode'] = 400;
+				  err['code'] = 'ROLE_NAME_WRONG';
+				  throw err;
+			  }
+			  var company:any = await caller.company();
+			  if (company) {
+				  ctx.args.data.companyId = company.id;
+				  ctx.args.data.companyName = company.name;
+			  }
+			  ctx.args.data.dtrSenderId = caller.id;
+			  ctx.args.data.dtrSenderName = caller.fullName;
+			  if (ctx.args.data.BatchName && ctx.args.data.BatchName.length > 0) {
+				  await app.models.Batch.upsertWithWhere(
+					  {
+						  dtrSenderId: ctx.args.data.dtrSenderId,
+						  MachineNumber: ctx.args.data.MachineNumber,
+						  BatchName: ctx.args.data.BatchName
+					  },
+					  {
+						  dtrSenderId: ctx.args.data.dtrSenderId,
+						  dtrSenderName: ctx.args.data.dtrSenderName,
+						  companyId: ctx.args.data.companyId,
+						  companyName: ctx.args.data.companyName,
+						  MachineNumber: ctx.args.data.MachineNumber,
+						  BatchName: ctx.args.data.BatchName,
+						  MachineName: ctx.args.data.MachineName,
+						  Loading: ctx.args.data.Loading,
+						  Water_Vol1_Total: ctx.args.data.Water_Vol1_Total,
+						  Water_Vol2_Total: ctx.args.data.Water_Vol2_Total,
+						  Water_Vol3_Total: ctx.args.data.Water_Vol3_Total,
+						  Water_Vol4_Total: ctx.args.data.Water_Vol4_Total,
+						  Steam_Vol_Total: ctx.args.data.Steam_Vol_Total,
+						  Power_Total: ctx.args.data.Power_Total,
+						  completed: 0,
+						  updatedAt: new Date()
+					  }
+				  );
+			  }
 
-	  app.models.Customer.findById(accessToken.userId, {}, function (err:any, caller:any) {
-	    if (err || !caller) return next(err);
-	    if (caller.roleName != ROLES.DTR){
-		    var err:any = new Error('The role is wrong');
-		    err['statusCode'] = 400;
-		    err['code'] = 'ROLE_NAME_WRONG';
-		    return next(err);
-		}
-		caller.company(function(err:any,company:any){
-			if (err) return next(err);
-			if (company){		
-				ctx.args.data.companyId = company.id;
-				ctx.args.data.companyName = company.name;
-			}		
-			ctx.args.data.dtrSenderId = caller.id;		
-			ctx.args.data.dtrSenderName = caller.fullName;
-			if (ctx.args.data.BatchName && ctx.args.data.BatchName.length>0){
-				app.models.Batch.upsertWithWhere(
-					{
-						dtrSenderId:ctx.args.data.dtrSenderId,
-						MachineNumber:ctx.args.data.MachineNumber,
-						BatchName:ctx.args.data.BatchName
-					},
-					{
-						dtrSenderId:ctx.args.data.dtrSenderId,
-						dtrSenderName:ctx.args.data.dtrSenderName,
-						companyId:ctx.args.data.companyId,
-						companyName:ctx.args.data.companyName,
-						MachineNumber:ctx.args.data.MachineNumber,
-						BatchName:ctx.args.data.BatchName,
-						MachineName:ctx.args.data.MachineName,
-						Loading:ctx.args.data.Loading,
-						Water_Vol1_Total:ctx.args.data.Water_Vol1_Total,
-						Water_Vol2_Total:ctx.args.data.Water_Vol2_Total,
-						Water_Vol3_Total:ctx.args.data.Water_Vol3_Total,
-						Water_Vol4_Total:ctx.args.data.Water_Vol4_Total,
-						Steam_Vol_Total:ctx.args.data.Steam_Vol_Total,
-						Power_Total:ctx.args.data.Power_Total,
-						completed:0,
-						updatedAt:new Date()
-					},
-					function(err:any){
-						if (err) return next(err);
-						app.models.Machine.upsertWithWhere(
-							{
-								dtrSenderId:ctx.args.data.dtrSenderId,
-								MachineNumber:ctx.args.data.MachineNumber
-							},
-							Object.assign({updatedAt:new Date()},ctx.args.data),						
-							next
-						);
-					}
-				);
-			}else{
-				app.models.Machine.upsertWithWhere(
-					{
-						dtrSenderId:ctx.args.data.dtrSenderId,
-						MachineNumber:ctx.args.data.MachineNumber
-					},
-					Object.assign({updatedAt:new Date()},ctx.args.data),						
-					next
-				);
-			}
-						
-		});		
-	  });
+			  await app.models.Machine.upsertWithWhere(
+				  {
+					  dtrSenderId: ctx.args.data.dtrSenderId,
+					  MachineNumber: ctx.args.data.MachineNumber
+				  },
+				  Object.assign({ updatedAt: new Date() }, ctx.args.data)
+			  );
 
+		  } catch (err) {
+			  console.dir(err);
+		  }	
+		  //should call next() here or data change in async function will be lost 
+		  next();	
+	  };
+
+	  func();	  
 	});
 }
--- server/models/machine.ts
+++ server/models/machine.ts
@@ -3,47 +3,55 @@ var app = require('../../server/server');
 export = function(Machine:any) {
 	Machine.observe('before save', (ctx:any, next:Function) => {
 		if (!ctx.where || !ctx.data) return next();
-		Machine.findOne({where:ctx.where},(err:any,currentInstance:any)=>{
-			let temp = ctx.data.MachineMainTemp?Number(ctx.data.MachineMainTemp):0;
-			if (currentInstance){
-				//when batch is switched, then the batch is completed
-				if (currentInstance.BatchName && currentInstance.BatchName.length>0 && currentInstance.BatchName != ctx.data.BatchName){
-					app.models.Batch.upsertWithWhere(
-						{
-							dtrSenderId:currentInstance.dtrSenderId,
-							MachineNumber:currentInstance.MachineNumber,
-							BatchName:currentInstance.BatchName
-						},
-						{
-							dtrSenderId:currentInstance.dtrSenderId,
-							dtrSenderName:currentInstance.dtrSenderName,
-							companyId:currentInstance.companyId,
-							companyName:currentInstance.companyName,
-							MachineNumber:currentInstance.MachineNumber,
-							BatchName:currentInstance.BatchName,
-							MachineName:currentInstance.MachineName,
-							Loading:currentInstance.Loading,
-							Water_Vol1_Total:currentInstance.Water_Vol1_Total,
-							Water_Vol2_Total:currentInstance.Water_Vol2_Total,
-							Water_Vol3_Total:currentInstance.Water_Vol3_Total,
-							Water_Vol4_Total:currentInstance.Water_Vol4_Total,
-							Steam_Vol_Total:currentInstance.Steam_Vol_Total,
-							Power_Total:currentInstance.Power_Total,
-							completed:1,
-							updatedAt:new Date()
-						},
-						(err:any)=>{console.dir(err)}
-					);
-				}
-				//logic to compose temperature array
-				let tempArray = currentInstance.tempArray?currentInstance.tempArray:[];
-				tempArray.push({x:ctx.data.updatedAt,y:temp});
-				if (tempArray.length>120) tempArray.shift()
-				ctx.data.tempArray = tempArray;
-			} else {
-				ctx.data.tempArray = [{x:ctx.data.updatedAt,y:temp}]
-			}			
-			next(err);
-		});
+
+		async function func(){
+			try{
+				let currentInstance:any = await Machine.findOne({where:ctx.where});
+				let temp = ctx.data.MachineMainTemp?Number(ctx.data.MachineMainTemp):0;
+				if (currentInstance){
+					//when batch is switched, then the batch is completed
+					if (currentInstance.BatchName && currentInstance.BatchName.length>0 && currentInstance.BatchName != ctx.data.BatchName){
+						await app.models.Batch.upsertWithWhere(
+							{
+								dtrSenderId:currentInstance.dtrSenderId,
+								MachineNumber:currentInstance.MachineNumber,
+								BatchName:currentInstance.BatchName
+							},
+							{
+								dtrSenderId:currentInstance.dtrSenderId,
+								dtrSenderName:currentInstance.dtrSenderName,
+								companyId:currentInstance.companyId,
+								companyName:currentInstance.companyName,
+								MachineNumber:currentInstance.MachineNumber,
+								BatchName:currentInstance.BatchName,
+								MachineName:currentInstance.MachineName,
+								Loading:currentInstance.Loading,
+								Water_Vol1_Total:currentInstance.Water_Vol1_Total,
+								Water_Vol2_Total:currentInstance.Water_Vol2_Total,
+								Water_Vol3_Total:currentInstance.Water_Vol3_Total,
+								Water_Vol4_Total:currentInstance.Water_Vol4_Total,
+								Steam_Vol_Total:currentInstance.Steam_Vol_Total,
+								Power_Total:currentInstance.Power_Total,
+								completed:1,
+								updatedAt:new Date()
+							}
+						);
+					}
+					//logic to compose temperature array
+					let tempArray = currentInstance.tempArray?currentInstance.tempArray:[];
+					tempArray.push({x:ctx.data.updatedAt,y:temp});
+					if (tempArray.length>120) tempArray.shift()
+					ctx.data.tempArray = tempArray;					
+				}else{
+					ctx.data.tempArray = [{x:ctx.data.updatedAt,y:temp}]
+				}				
+			}catch(err){
+				console.dir(err);
+			}
+			//should call next() here or data change in async function will be lost 
+			next();		
+		};
+
+		func();
 	});
 }
