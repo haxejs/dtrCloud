8bdea30a49327f197a22d273c6a7be13be2a5ec6
Richard Shi <kmshi_array@yahoo.com>
2017-12-11 01:12:51 +0800

    send alarm notifications

--- server/models/machine.ts
+++ server/models/machine.ts
@@ -1,8 +1,18 @@
 var app = require('../../server/server');
+import {Push,DeviceType,MsgType,DeployStatus} from '@xialeistudio/baidu-push';
 
 export = function(Machine:any) {
 	Machine.observe('before save', (ctx:any, next:Function) => {
 		if (!ctx.where || !ctx.data) return next();
+
+		function sendPushNotifications(tag:string,title:string,description:string){
+			let baiduPushConfig = app.get("baiduPush");
+			let sdk = new Push(baiduPushConfig.apiKey, baiduPushConfig.secretKey);
+			let msg = sdk.buildMessage(DeviceType.Android, title, description);
+			sdk.pushGroup(tag, msg, DeviceType.Android, MsgType.Notification, DeployStatus.Development)
+				.then(val=>console.dir(val))
+				.catch(err => console.dir(err));
+		}
 
 		async function func(){
 			try{
@@ -22,13 +32,21 @@ export = function(Machine:any) {
 							}
 						);
 					}
+					//when MainAlarm is changed, send an alarm
+					if (ctx.data.companyId && ctx.data.Main_Alarm && currentInstance.Main_Alarm != ctx.data.Main_Alarm) {
+						sendPushNotifications('' + ctx.data.companyId,'Main Alarm',ctx.data.Main_Alarm + '');
+					}
 					//logic to compose temperature array
 					let tempArray = currentInstance.tempArray?currentInstance.tempArray:[];
 					tempArray.push({x:ctx.data.updatedAt,y:temp});
 					if (tempArray.length>120) tempArray.shift()
 					ctx.data.tempArray = tempArray;					
 				}else{
-					ctx.data.tempArray = [{x:ctx.data.updatedAt,y:temp}]
+					ctx.data.tempArray = [{x:ctx.data.updatedAt,y:temp}];
+					//MainAlarm is here
+					if (ctx.data.companyId && ctx.data.Main_Alarm) {
+						sendPushNotifications('' + ctx.data.companyId,'Main Alarm',ctx.data.Main_Alarm + '');
+					}
 				}				
 			}catch(err){
 				console.dir(err);
